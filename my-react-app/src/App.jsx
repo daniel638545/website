8f0739ebd68d3d70d997170eb4eae130075e8e9d
Daniel <konto1chat2345@email.com>
2025-11-19 21:46:58 +0100

    Initialize my-react-app with Vite, React, and ESLint setup; add Header component and basic styling.

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+// import Header from "./Header" to tez dziala
+import Header from "./Header.jsx"
+function App() {
+  
+  return (
+    // <Header></Header> to tez dziala
+    <Header/>
+  )
+
+}
+
+export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,25 @@
+function Header(){
+
+return(
+    <header>
+        <h1> My website Zioom!!! </h1>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="aaaa#">Home1</a>
+                </li>
+
+                <li>
+                   <a href="https://www.google.com/search?q=dzwon&sca_esv=55588fd05011d482&sxsrf=AE3TifNACraPtzEkNhul-xgwXVI0iLR2Aw%3A1763577028917&source=hp&ei=xAweafGaNrmVxc8P7svP4A8&iflsig=AOw8s4IAAAAAaR4a1K5lFnVPN9Pfk_uG83CKQ3Z8G7Kz&ved=0ahUKEwjxjN2y7P6QAxW5SvEDHe7lE_wQ4dUDCBg&uact=5&oq=dzwon&gs_lp=Egdnd3Mtd2l6IgVkendvbjILEC4YgAQYywEY1AIyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCxAuGIAEGMsBGNQCMggQABiABBjLATIIEAAYgAQYywFI2whQAFiaBnAAeACQAQCYAWOgAfgCqgEBNbgBA8gBAPgBAZgCBaAChQPCAgoQIxiABBiKBRgnwgIEECMYJ8ICCxAAGIAEGIoFGJECwgIFEAAYgATCAgsQLhjHARjRAxiABMICBRAuGIAEwgIIEC4YgAQY1ALCAgsQLhiABBjHARivAZgDAJIHAzQuMaAH2C2yBwM0LjG4B4UDwgcFMC40LjHIBwk&sclient=gws-wiz"> Dzwon</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+
+);
+
+}
+
+export default Header;
